6e61797ad0f140ddac34b8a0304c76d9a4c4f7f2
prabhath6 <prabhathkiran20@gmail.com>
2016-03-19 22:34:10 -0700

    Add post_data to fill in the form and get the html file

---
+++ indeed/post_using_mechanize.py
@@ -0,0 +1,26 @@
+from mechanize import urljoin, urlopen, ParseResponse
+
+
+def post_data(url_add, **kwargs):
+
+    # search based on job title and place
+    query = "jobs?q=&l="
+
+    # open the absloute url
+    response = urlopen(urljoin(url, query))
+
+    # select the form
+    forms = ParseResponse(response, backwards_compat=False)
+    form = forms[0]
+
+    # fill the form
+    form["q"] = kwargs["q"]
+    form["l"] = kwargs["l"]
+
+    return urlopen(form.click()).read()
+
+if __name__ == "__main__":
+    # url to post the data.
+    url = "http://www.indeed.com"
+
+    post_data(url, {"q": "Python Developer", "l": "Santa Clara, CA"})
